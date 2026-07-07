b73d7a24528ded76a2ce08e27b07423b200278b8
Trudigard <christina.brodowsky@gmail.com>
2026-07-07 16:37:25 +0200

    make aero_state inout for update_bin

--- src/chemistry/aerosol/modal_aerosol_state_mod.F90
+++ src/chemistry/aerosol/modal_aerosol_state_mod.F90
@@ -372,7 +372,7 @@ contains
   !------------------------------------------------------------------------------
   !------------------------------------------------------------------------------
   subroutine update_bin( self, bin_ndx, col_ndx, lyr_ndx, delmmr_sum, delnum_sum, tnd_ndx, dtime, tend )
-    class(modal_aerosol_state), intent(in) :: self
+    class(modal_aerosol_state), intent(inout) :: self
     integer, intent(in) :: bin_ndx                ! bin number
     integer, intent(in) :: col_ndx                ! column index
     integer, intent(in) :: lyr_ndx                ! vertical layer index
